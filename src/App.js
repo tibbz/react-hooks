a37b7a6bd152ed26b2ba9e66195f91b0d5a2c365
Tibbers <sarahgaco@gmail.com>
2022-05-03 21:01:23 +0200

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React from 'react'
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is my CRUD App.
-        </p>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
